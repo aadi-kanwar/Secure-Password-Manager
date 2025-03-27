acb264d1ccef8298ca4dd95b7b41a20d9e00ab49
Aadi Kanwar <aadi.kanwar@gmail.com>
2025-03-27 10:17:58 +0530

    New_Idea

--- backend/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "mysecret")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://user:password@localhost/password_manager")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- frontend/src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const API_URL = "http://localhost:5000";
-
-export const registerUser = (user) => axios.post(`${API_URL}/register`, user);
-export const storePassword = (passwordData) => axios.post(`${API_URL}/store-password`, passwordData);
-export const getPasswords = (userId) => axios.get(`${API_URL}/get-passwords/${userId}`);
